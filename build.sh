bcdf1767206ee8de5e756a9225ceb02046cdacdf
Ralph Alexander Bariz <ralph@localhost.localdomain>
2017-05-22 12:57:26 +0200

    updated build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rootDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $rootDir/base && dub test &&
-cd $rootDir/util && dub test &&
-cd $rootDir/alien && dub test &&
-
-cd $rootDir/causal && dub build --build $1 ${@:2}
